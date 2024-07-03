fbf8bd3abf4f5aa57944ce27eca39eaa193de4ba
shawukie4them <shawukiekawuki@gmail.com>
2024-07-03 11:21:24 +0300

    scope

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+//Exercise 1
+var customerName = 'bob';
+
+//Exercise 2
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+//Exercise 3
+function setBestCustomer() {
+    bestCustomer = 'not bob'; //global variable
+    console.log('setBestCustomer:', bestCustomer);
+}
+
+//Exercise 4
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    console.log('overwriteBestCustomer:', bestCustomer);
+}
+
+//Exercise 5
+const leastFavoriteCustomer = 'someone else';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'bob';
+}
+
+
